0916e8853623ee4c61eeaccb9f57f9728ddcd492
Marcel Castro <46653213+marcelferran@users.noreply.github.com>
2025-04-22 14:38:03 -0600

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 # Configuraciones predeterminadas
-DEFAULT_PLOT_SIZE = (12, 10)  # Tamaño por defecto de las gráficas
+DEFAULT_PLOT_SIZE = (8, 6)  # Tamaño por defecto de las gráficas
 DEFAULT_COLOR_PALETTE = 'blues'  # Paleta de colores por defecto para las gráficas
 
 # Función para obtener la clave de API
