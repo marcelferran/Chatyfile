29897d62eecb20f2775351cd4e3ac4ac38ac5a1c
Marcel Castro <46653213+marcelferran@users.noreply.github.com>
2025-04-22 14:40:04 -0600

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,5 @@
 import streamlit as st
 
-# Configuraciones predeterminadas
-DEFAULT_PLOT_SIZE = (8, 6)  # Tamaño por defecto de las gráficas
-DEFAULT_COLOR_PALETTE = 'blues'  # Paleta de colores por defecto para las gráficas
-
 def configure_genai(api_key):
     if api_key:
         import google.generativeai as genai
