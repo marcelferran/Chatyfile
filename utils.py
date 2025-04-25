aa6463badb7a38d08df808bbe9f891ce168548ee
Marcel Castro <46653213+marcelferran@users.noreply.github.com>
2025-04-25 13:13:31 -0600

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,11 +1,8 @@
 import pandas as pd
-import streamlit as st
 
-def mostrar_resumen_df(df):
-    st.success("✅ Archivo cargado correctamente.")
-    num_rows, num_cols = df.shape
-    st.write("**Resumen del archivo:**")
-    st.write(f"- Número de filas: {num_rows}")
-    st.write(f"- Número de columnas: {num_cols}")
-    st.write("**Nombres de las columnas:**")
-    st.dataframe(pd.DataFrame(df.columns, columns=["Columnas"]))
+def cargar_csv(uploaded_file):
+    try:
+        df = pd.read_csv(uploaded_file)
+        return df
+    except Exception as e:
+        return None
