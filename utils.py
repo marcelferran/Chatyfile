57d9ed914da4e14c343d7818f9c398174bd0f01d
Marcel Castro <46653213+marcelferran@users.noreply.github.com>
2025-04-28 20:38:18 -0600

    Update utils.py

--- utils.py
+++ utils.py
@@ -8,4 +8,4 @@ def mostrar_resumen_df(df):
     st.write(f"- Número de filas: {num_rows}")
     st.write(f"- Número de columnas: {num_cols}")
     st.write("**Muestra aleatoria de 10 filas:**")
-    st.dataframe(df.sample(10))
+    st.dataframe(df.head(6))
