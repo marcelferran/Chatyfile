04fec2de19992037d65b73a777939c189d9eebc3
Marcel Castro <46653213+marcelferran@users.noreply.github.com>
2025-05-23 18:53:02 -0600

    Update config.py

--- config.py
+++ config.py
@@ -7,5 +7,5 @@ def configure_genai():
         genai.configure(api_key=api_key)
         return True
     except KeyError:
-        st.error("❌ No se encontró la clave API en `st.secrets`. Por favor, configura `GOOGLE_API_KEY` en los secrets de Streamlit.")
+        st.error("❌ No se encontró la clave API en `st.secrets`. Por favor, configura `GOOGLE_API_KEY` en los secrets de Streamlit")
         st.stop()
